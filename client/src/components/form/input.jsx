76ba1b309d7461c7642243ceca77351f1861f14e
Manav Misra <manavm@visionwebsoft.com>
2023-03-24 09:50:18 -0500

    fix: add missing 'type' prop

--- client/src/components/form/input.jsx
+++ client/src/components/form/input.jsx
@@ -1,7 +1,7 @@
 import camelCase from "lodash/camelCase";
 import PropTypes from "prop-types";
 
-export default function Input({ label, id, placeholder, required }) {
+export default function Input({ label, id, type, placeholder, required }) {
   return (
     <div>
       <label htmlFor={id} className="sr-only">
@@ -9,6 +9,7 @@ export default function Input({ label, id, placeholder, required }) {
       </label>
       <input
         id={id}
+        type={type}
         placeholder={placeholder || label}
         required={required}
         name={camelCase(id)}
@@ -18,6 +19,7 @@ export default function Input({ label, id, placeholder, required }) {
 }
 
 Input.defaultProps = {
+  type: "text",
   placeholder: null,
   required: true,
 };
@@ -25,6 +27,7 @@ Input.defaultProps = {
 Input.propTypes = {
   label: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  type: PropTypes.string,
   placeholder: PropTypes.string,
   required: PropTypes.bool,
 };
